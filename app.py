9c3d2cf26f2869e3749262a35760cc75f552a818
Balázs Hosszu <hosszub@adnovum.hu>
2019-03-09 16:18:40 +0100

    Add FPS counter
    This is pretty noisy and not really useful.
    However, this is a trendy thing to do
    for OpenCV projects, it seems.
    Lets add it for now and observe its usability.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import numpy
 import cv2
 from mss import mss
@@ -5,10 +6,14 @@ from mss import mss
 monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
 
 sct = mss()
+print("Press q to exit.")
 
 while 1:
+    last_time = time.time()
     img = numpy.array(sct.grab(monitor))
     cv2.imshow('test', img)
+    print("fps: {}".format(1 / (time.time() - last_time)))
+
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
