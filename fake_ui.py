af2538b5e791e902e72eca51d25cd09ca97a6358
Balázs Hosszu <hbaloo28@gmail.com>
2019-03-14 09:43:50 +0100

    Style changes in fake_ui.py

--- fake_ui.py
+++ fake_ui.py
@@ -6,19 +6,17 @@ import time
 
 # Please check MSDN documentation on these WIN32 codes
 # Looks scary, but really not so bad
+# Button events
 WM_LBUTTONDOWN = 0x0201
 WM_LBUTTONUP = 0x0202
 MK_LBUTTON = 0x0001
-
+# Keyboard events
 WM_KEYDOWN = 0x0100
-# WM_KEYUP = 0x0101
-# WM_CHAR = 0x0102
-
+WM_KEYUP = 0x0101
+WM_CHAR = 0x0102
 # Virtual code and scan code for the letter 'c'
 C_VK = 0x43
 C_SC = 0x2E
-
-
 
 # Coordinates for clicking. This part is unstable.
 # If no OpenCV window is open, the coordinates are different inside Diablo, for some reason.
@@ -34,7 +32,8 @@ def initalize_window_handler(window_title='DIABLO'):
         sys.exit("Diablo was not found. Please makes sure Diablo is running, and you are running this program with sufficient rights.")
     return WHND
 
-def press_button(whnd,virtual_key, scan_code):
+
+def press_button(whnd, virtual_key, scan_code):
     if whnd == 0:
         sys.exit(
             "Window handle not configfured. Please use initalize_window_handle().")
@@ -47,7 +46,7 @@ def press_button(whnd,virtual_key, scan_code):
     win32gui.PostMessage(whnd, WM_KEYDOWN, virtual_key, key_down)
 
 
-def left_click(whnd,coordinates):
+def left_click(whnd, coordinates):
     if whnd == 0:
         sys.exit(
             "Window handle not configfured. Please use initalize_window_handle().")
