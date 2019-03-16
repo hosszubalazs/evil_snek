9a2c816a84750877ba74a865605d3c8a84be714d
Balázs Hosszu <hbaloo28@gmail.com>
2019-03-16 12:59:46 +0100

    Set TESSDATA path for win devs
    the default is not correct, easy to forget

--- activate_env.bat
+++ activate_env.bat
@@ -1,3 +1,4 @@
 C:\Users\hosszub\AppData\Local\Programs\Python\Python37-32\python.exe -m pip install virtualenv
 C:\Users\hosszub\AppData\Local\Programs\Python\Python37-32\python.exe -m virtualenv venv
 venv\Scripts\activate
+set TESSDATA_PREFIX="C:\Program Files (x86)\Tesseract-OCR\tessdata"
