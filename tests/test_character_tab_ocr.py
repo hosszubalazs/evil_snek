d90b40bcc472da9f207f60487e299b3e220d99d9
Balázs Hosszu <hosszub@adnovum.hu>
2019-03-16 16:10:59 +0100

    Restructure test data
    put everything into a nice new folder

--- tests/test_character_tab_ocr.py
+++ tests/test_character_tab_ocr.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy
 from evil_snek import evil_ocr, devil_vision
+from tests import TEST_DATA_PATH
 
 
 def test_ocr_xp_0():
-    img = cv2.imread('tests/character_screen.png')
+    img = cv2.imread(TEST_DATA_PATH+'character_screen.png')
     xp_picture = devil_vision.crop_xp(img)
     xp_value = evil_ocr.read_single_int(xp_picture)
     assert xp_value == 73623
--- tests/test_evil_ocr.py
+++ tests/test_evil_ocr.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 from evil_snek import evil_ocr as ocr
+from tests import TEST_DATA_PATH
 
 # plans:
 # Input Chracter tab screen
@@ -9,18 +10,18 @@ from evil_snek import evil_ocr as ocr
 
 
 def test_ocr_xp_0():
-    img = cv2.imread('tests/xp_3447.png')
+    img = cv2.imread(TEST_DATA_PATH+'xp_3447.png')
     result = ocr.read_single_int(img)
     assert result == 3447
 
 
 def test_ocr_xp_1():
-    img = cv2.imread('tests/xp_73623.png')
+    img = cv2.imread(TEST_DATA_PATH+'xp_73623.png')
     result = ocr.read_single_int(img)
     assert result == 73623
 
 
 def test_ocr_xp_2():
-    img = cv2.imread('tests/xp_83419.png')
+    img = cv2.imread(TEST_DATA_PATH+'xp_83419.png')
     result = ocr.read_single_int(img)
     assert result == 83419
