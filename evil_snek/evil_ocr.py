99fc5066a1cd53449defb58d83248f770533d8bf
Balázs Hosszu <hbaloo28@gmail.com>
2019-03-16 10:53:43 +0100

    Include OCR in testing
    Actually write a useful test.
    
    Docker and pipeline needs to be updated. This test wont be able to run on the windows agents,
    as no tesseract is autoamted there.
    It will run inside docker on a linux host.

--- evil_snek/evil_ocr.py
+++ evil_snek/evil_ocr.py
@@ -1,6 +1,18 @@
 import pytesseract
+import logging
 
 
-def read_single_number(image):
-    return pytesseract.image_to_string(
+def read_single_int(image):
+    recognized_text = pytesseract.image_to_string(
         image, lang="digits_comma", config="--psm 8")
+    # trim the single quotes from the two sides
+    trimmed = recognized_text.strip('\'')
+    # Default for 0
+    value = 0
+    # validate for int
+    try:
+        value = int(trimmed)
+    except ValueError:
+        logging.warning('Unsuccessful read_single_int')
+
+    return value
--- tests/test_evil_ocr.py
+++ tests/test_evil_ocr.py
@@ -1,12 +1,22 @@
+import cv2
+import numpy
+from evil_snek import evil_ocr as ocr
+from PIL import Image
 
+# plans:
+# Input Chracter tab screen
+# to assert : experience is correctly calculated
+# d development ideas :  all fields will be necessary
 
-
- ## Input Chracter tab screen
-## to assert : experience is correctly calculated
-##d development ideas :  all fields will be necessary
-
-
-## moar tests :  more test data screenshot, especially around problems!
 
 def test_pytest_alive():
     assert 5 == 5
+
+
+def test_pytest_alive():
+    # cv2.imread('xp_captured.png',0)
+    img = Image.open('tests/xp_captured.png')
+    #img_new = Image.fromarray(img)
+    result = ocr.read_single_int(img)
+    print(result)
+    assert result == 3447
