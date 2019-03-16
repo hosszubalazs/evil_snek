aa963501d4f645844d0221d87154c15660139e57
Balázs Hosszu <hosszub@adnovum.hu>
2019-03-16 16:04:07 +0100

    Remove PIL from testing
    opencv is also good for  reading image files

--- tests/test_evil_ocr.py
+++ tests/test_evil_ocr.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy
 from evil_snek import evil_ocr as ocr
-from PIL import Image
 
 # plans:
 # Input Chracter tab screen
@@ -9,16 +8,16 @@ from PIL import Image
 # d development ideas :  all fields will be necessary
 
 def test_ocr_xp_0():
-    img = Image.open('tests/xp_3447.png')
+    img = cv2.imread('tests/xp_3447.png')
     result = ocr.read_single_int(img)
     assert result == 3447
 
 def test_ocr_xp_1():
-    img = Image.open('tests/xp_73623.png')
+    img = cv2.imread('tests/xp_73623.png')
     result = ocr.read_single_int(img)
     assert result == 73623
 
 def test_ocr_xp_2():
-    img = Image.open('tests/xp_83419.png')
+    img = cv2.imread('tests/xp_83419.png')
     result = ocr.read_single_int(img)
     assert result == 83419
