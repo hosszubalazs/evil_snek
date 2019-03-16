bee88169112b7f6dbe16c38161bbea41a6a9db99
Balázs Hosszu <hosszub@adnovum.hu>
2019-03-16 16:06:13 +0100

    Write test for cropping XP, OCR-ing it

---
+++ tests/test_character_tab_ocr.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy
+from evil_snek import evil_ocr, devil_vision
+
+
+def test_ocr_xp_0():
+    img = cv2.imread('tests/character_screen.png')
+    xp_picture = devil_vision.crop_xp(img)
+    xp_value = evil_ocr.read_single_int(xp_picture)
+    assert xp_value == 73623
--- tests/test_evil_ocr.py
+++ tests/test_evil_ocr.py
@@ -7,15 +7,18 @@ from evil_snek import evil_ocr as ocr
 # to assert : experience is correctly calculated
 # d development ideas :  all fields will be necessary
 
+
 def test_ocr_xp_0():
     img = cv2.imread('tests/xp_3447.png')
     result = ocr.read_single_int(img)
     assert result == 3447
 
+
 def test_ocr_xp_1():
     img = cv2.imread('tests/xp_73623.png')
     result = ocr.read_single_int(img)
     assert result == 73623
+
 
 def test_ocr_xp_2():
     img = cv2.imread('tests/xp_83419.png')
