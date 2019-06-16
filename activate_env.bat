858ec154ae1e73146338937ff3a15a79a1759120
Balázs Hosszu <hbaloo28@gmail.com>
2019-06-16 09:56:48 +0200

    Remove old referencere to Tesseract

--- activate_env.bat
+++ activate_env.bat
@@ -1,4 +1,3 @@
 C:\Users\hosszub\AppData\Local\Programs\Python\Python37-32\python.exe -m pip install virtualenv
 C:\Users\hosszub\AppData\Local\Programs\Python\Python37-32\python.exe -m virtualenv venv
 venv\Scripts\activate
-set TESSDATA_PREFIX="C:\Program Files (x86)\Tesseract-OCR\tessdata"
