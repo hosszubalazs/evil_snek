ad479eb5527b1e90ea3af3a30387702f9ffd664b
Balázs Hosszu <hbaloo28@gmail.com>
2019-06-23 10:11:48 +0200

    update belt testing

---
+++ tests/test_belt_accuracy.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy
+from evil_snek import devil_vision
+from tests import TEST_DATA_PATH
+
+
+def test_health_potion_detection_1440_0():
+    expected = [0, 0, 0, 1, 1, 1, 2, 2]
+    file_name = 'character_screen_1440_0.png'
+    screenshot = cv2.imread(TEST_DATA_PATH+file_name)
+
+    actual = devil_vision.get_health_in_belt(screenshot)
+
+    assert actual == expected
+
+
+def test_health_potion_detection_1440_1():
+    expected = [1, 1, 1, 1, 1, 1, 2, 2]
+    file_name = 'character_screen_1440_1.png'
+    screenshot = cv2.imread(TEST_DATA_PATH+file_name)
+
+    actual = devil_vision.get_health_in_belt(screenshot)
+
+    assert actual == expected
